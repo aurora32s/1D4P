c89126cad680d194774ff67a85e30e1734e3549c
aurora32s <aurora32s@naver.com>
2023-04-03 16:52:32 +0900

    [BASE] #4 - convention 관리를 위한 build-logic 추가

---
+++ build-logic/convention/src/main/kotlin/ext/AndroidCompose.kt
@@ -0,0 +1,29 @@
+package ext
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+/**
+ * App Module Configuration
+ */
+@Suppress("UnstableApiUsage")
+internal fun Project.configureAndroidCompose(
+    commonExtension: CommonExtension<*, *, *, *>
+) {
+    val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+    commonExtension.apply {
+        buildFeatures {
+            compose = true
+        }
+        composeOptions {
+            kotlinCompilerExtensionVersion =
+                libs.findVersion("compose-kotlin-compiler").get().toString()
+        }
+    }
+    dependencies {
+        "implementation"(libs.findBundle("compose").get())
+    }
+}

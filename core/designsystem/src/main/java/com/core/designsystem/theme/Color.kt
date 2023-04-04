3bad90c935d79864ba88316477508cf6580c2284
aurora32s <aurora32s@naver.com>
2023-04-04 13:18:46 +0900

    [BASE] #7 - theme.color 추가

---
+++ core/designsystem/src/main/java/com/core/designsystem/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PinkA400 = Color(0xFFF50057)
+val DeepBlue800 = Color(0xFF0A104D)
+val DeepBlue900 = Color(0xFF000536)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)

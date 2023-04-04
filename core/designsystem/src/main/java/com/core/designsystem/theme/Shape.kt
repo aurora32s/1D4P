e261017b77c6e56834c725b079e1da8aeaaeaf63
aurora32s <aurora32s@naver.com>
2023-04-04 14:09:37 +0900

    [BASE] #7 - theme.shape 추가

--- core/designsystem/src/main/java/com/core/designsystem/theme/Shape.kt
+++ core/designsystem/src/main/java/com/core/designsystem/theme/Shape.kt
@@ -1,7 +1,11 @@
 package com.core.designsystem.theme
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-
+    small = RoundedCornerShape(percent = 50),
+    medium = RoundedCornerShape(size = 6.dp),
+    large = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp)
 )

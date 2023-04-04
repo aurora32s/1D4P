461df7a84144eca5806db14853567e6002db9801
aurora32s <aurora32s@naver.com>
2023-04-04 13:30:34 +0900

    [BASE] #7 - theme.typography 추가

---
+++ core/designsystem/src/main/java/com/core/designsystem/theme/Type.kt
@@ -0,0 +1,53 @@
+package com.core.designsystem.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.core.designsystem.R
+
+private val BM = FontFamily(
+    Font(R.font.BMDOHYEON, weight = FontWeight.Bold),
+    Font(R.font.BMHANNAPro, weight = FontWeight.Medium),
+    Font(R.font.BMHANNAAir, weight = FontWeight.Light)
+)
+
+val Typography = Typography(
+    h2 = TextStyle(
+        fontFamily = BM,
+        fontSize = 60.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    h4 = TextStyle(
+        fontFamily = BM,
+        fontSize = 33.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    h5 = TextStyle(
+        fontFamily = BM,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    h6 = TextStyle(
+        fontFamily = BM,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = BM,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    body1 = TextStyle(
+        fontFamily = BM,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    body2 = TextStyle(
+        fontFamily = BM,
+        fontSize = 10.sp,
+        fontWeight = FontWeight.Light
+    )
+)

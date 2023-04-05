0e90ef4aecbf255b525af72460cb3265b9c3e68a
aurora32s <aurora32s@naver.com>
2023-04-05 22:40:45 +0900

    [FEAT] #11 - PostLocalDataSourceImpl.kt의 기본 동작 코드 추가

--- core/datasource/src/main/java/com/core/datasource/model/Post.kt
+++ core/datasource/src/main/java/com/core/datasource/model/Post.kt
@@ -1,14 +1,24 @@
 package com.core.datasource.model
 
+import com.core.database.model.PostEntity
+
 /**
  * data source Post 정보
  */
 data class Post(
-    val id: String, // post id
+    val id: Int?, // post id
     val year: Int, // 연도
     val month: Int, // 월
     val day: Int, // 일
     val content: String?, // 내용
     val images: List<Image>, // 이미지
     val tags: List<Tag> // 태그
+)
+
+internal fun Post.toPostEntity() = PostEntity(
+    id = id,
+    year = year,
+    month = month,
+    day = day,
+    content = content
 )

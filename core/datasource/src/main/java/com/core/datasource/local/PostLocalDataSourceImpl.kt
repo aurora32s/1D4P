501c4d4e34ef92666124a9ec7a1f7020a3e441b9
aurora32s <aurora32s@naver.com>
2023-04-05 23:11:56 +0900

    [FEAT] #11 - PostDatasource 비동기 작업 추가

--- core/datasource/src/main/java/com/core/datasource/local/PostLocalDataSourceImpl.kt
+++ core/datasource/src/main/java/com/core/datasource/local/PostLocalDataSourceImpl.kt
@@ -6,53 +6,59 @@ import androidx.paging.PagingData
 import com.core.database.dao.PostDao
 import com.core.datasource.PostDatasource
 import com.core.datasource.model.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class PostLocalDataSourceImpl @Inject constructor(
     private val postDao: PostDao
 ) : PostDatasource {
-    override suspend fun addPost(post: Post): Int {
+    override suspend fun addPost(post: Post): Int = coroutineScope {
         val postId = postDao.insertPost(post.toPostEntity())
-        postDao.insertImages(post.images.map { it.toImageEntity(postId) })
-        postDao.insertTags(post.tags.map { it.toTagEntity(postId) })
-        return postId
+        launch { postDao.insertImages(post.images.map { it.toImageEntity(postId) }) }
+        launch { postDao.insertTags(post.tags.map { it.toTagEntity(postId) }) }
+        postId
     }
 
-    override suspend fun getPost(year: Int, month: Int, day: Int): Post? {
+    override suspend fun getPost(year: Int, month: Int, day: Int): Post? = coroutineScope {
         val post = postDao.selectPostByDate(year, month, day)
-        return post?.id?.let {
-            val images = postDao.selectImagesByPost(it)
-            val tags = postDao.selectTagsByPost(it)
+        post?.id?.let {
+            val images = async { postDao.selectImagesByPost(it) }
+            val tags = async { postDao.selectTagsByPost(it) }
             Post(
                 id = it,
                 year = post.year,
                 month = post.month,
                 day = post.day,
                 content = post.content,
-                images = images.map { image -> image.toImage() },
-                tags = tags.map { tag -> tag.toTag() }
+                images = images.await().map { image -> image.toImage() },
+                tags = tags.await().map { tag -> tag.toTag() }
             )
         }
     }
 
-    override suspend fun getPosts(year: Int, month: Int): List<Post> {
+    override suspend fun getPosts(year: Int, month: Int): List<Post> = coroutineScope {
         val posts = postDao.selectPostByMonth(year, month)
-        return posts.mapNotNull { post ->
-            post.id?.let {
-                val images = postDao.selectImagesByPost(it)
-                val tags = postDao.selectTagsByPost(it)
-                Post(
-                    id = it,
-                    year = post.year,
-                    month = post.month,
-                    day = post.day,
-                    content = post.content,
-                    images = images.map { image -> image.toImage() },
-                    tags = tags.map { tag -> tag.toTag() }
-                )
+        posts.map { post ->
+            async {
+                post.id?.let {
+                    val images = async { postDao.selectImagesByPost(it) }
+                    val tags = async { postDao.selectTagsByPost(it) }
+                    Post(
+                        id = it,
+                        year = post.year,
+                        month = post.month,
+                        day = post.day,
+                        content = post.content,
+                        images = images.await().map { image -> image.toImage() },
+                        tags = tags.await().map { tag -> tag.toTag() }
+                    )
+                }
             }
-        }
+        }.awaitAll().filterNotNull()
     }
 
     override fun getPostPaging(year: Int, month: Int): Flow<PagingData<Post>> {

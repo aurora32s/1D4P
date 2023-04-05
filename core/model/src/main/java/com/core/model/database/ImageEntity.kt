0c795bb23236cded15ae74afaee3e4909bad49a4
aurora32s <aurora32s@naver.com>
2023-04-05 20:32:38 +0900

    [FEAT] #11 - post table 과 image table 추가

--- core/model/src/main/java/com/core/model/database/ImageEntity.kt
+++ core/model/src/main/java/com/core/model/database/ImageEntity.kt
@@ -1,10 +1,30 @@
 package com.core.model.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
+import androidx.room.PrimaryKey
+
 /**
  * Image Table
  */
+@Entity(
+    tableName = "image",
+    foreignKeys = [
+        ForeignKey(
+            entity = PostEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["post_id"],
+            onDelete = CASCADE
+        )
+    ]
+)
 data class ImageEntity(
+    @PrimaryKey(autoGenerate = true)
     val id: String,
+    @ColumnInfo(name = "post_id")
     val postId: String,
+    @ColumnInfo(name = "image_url")
     val imageUrl: String
 )

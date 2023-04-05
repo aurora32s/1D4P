2fae62f4ad0fbb3052f6807299d492d5250d776e
aurora32s <aurora32s@naver.com>
2023-04-05 21:05:05 +0900

    [FEAT] #11 - 각 게시글(post)의 tag 정보를 위한 tag table 추가

---
+++ core/model/src/main/java/com/core/model/database/TagEntity.kt
@@ -0,0 +1,26 @@
+package com.core.model.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "tag",
+    foreignKeys = [
+        ForeignKey(
+            entity = PostEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["post_id"],
+            onDelete = CASCADE
+        )
+    ]
+)
+data class TagEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name = "post_id")
+    val postId: Int,
+    val name: String
+)

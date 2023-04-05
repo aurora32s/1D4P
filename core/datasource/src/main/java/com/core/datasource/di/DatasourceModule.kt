442fb442e4e4db69f4eb6bcc90d2afcafdc60de5
aurora32s <aurora32s@naver.com>
2023-04-05 22:59:39 +0900

    [FEAT] #11 - PostDatasource Hilt Module 추가

---
+++ core/datasource/src/main/java/com/core/datasource/di/DatasourceModule.kt
@@ -0,0 +1,17 @@
+package com.core.datasource.di
+
+import com.core.datasource.PostDatasource
+import com.core.datasource.local.PostLocalDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DatasourceModule {
+    @Binds
+    fun bindPostDatasource(
+        postLocalDataSourceImpl: PostLocalDataSourceImpl
+    ): PostDatasource
+}

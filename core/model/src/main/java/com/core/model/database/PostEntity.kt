82cba92d8796f8b0e849aa81a4fc4a57ae7b2e62
Yeoonju/Jane <aurora32s@naver.com>
2023-04-06 13:54:55 +0900

    Revert "[FEAT] #11 - post table 과 image table 추가"
    This reverts commit a2585b361efbcc38a21a4cb80352c5b9f271dc26.

--- core/model/src/main/java/com/core/model/database/PostEntity.kt
+++ core/model/src/main/java/com/core/model/database/PostEntity.kt
@@ -1,14 +1,9 @@
 package com.core.model.database
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
 /**
  * Post Table
  */
-@Entity(tableName = "post")
 data class PostEntity(
-    @PrimaryKey(autoGenerate = true)
     val id: String,
     val year: Int,
     val month: Int,

09622d7f9e0975b4be22545017811f7bf8eee419
Yeoonju/Jane <aurora32s@naver.com>
2023-04-06 13:54:55 +0900

    Revert "[FEAT] #11 - PostLocalDataSourceImpl.kt의 기본 동작 코드 추가"
    This reverts commit a44b6b2ce7c5d6bba50bd163d89eea635aab24c4.

--- core/datasource/src/main/java/com/core/datasource/model/Tag.kt
+++ core/datasource/src/main/java/com/core/datasource/model/Tag.kt
@@ -1,22 +1,9 @@
 package com.core.datasource.model
 
-import com.core.database.model.TagEntity
-
 /**
  * datasource tag 정보
  */
 data class Tag(
-    val id: Int?, // tag id
+    val id: Int, // tag id
     val name: String // tag 내용
-)
-
-internal fun Tag.toTagEntity(postId: Int) = TagEntity(
-    id = id,
-    postId = postId,
-    name = name
-)
-
-internal fun TagEntity.toTag() = Tag(
-    id = id,
-    name = name
 )

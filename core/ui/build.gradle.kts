0c500ab0cac82947b0da0239555d69bf6f395427
aurora32s <aurora32s@naver.com>
2023-04-06 21:22:39 +0900

    [FEAT] #13 - GalleryContainer 기본 코드 추가

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
-    id ("haroo.android.library")
+    id("haroo.android.library")
     id("haroo.android.library.compose")
 }
 
 android {
     namespace = "com.core.ui"
+}
+
+dependencies {
+    implementation(project(":core:model"))
+
+    implementation(libs.bundles.coil)
+    implementation(libs.paging.compose)
 }

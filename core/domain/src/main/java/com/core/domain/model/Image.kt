b14c2e0e6e154c5f03f3ebbb217a9a7dffd34db8
aurora32s <aurora32s@naver.com>
2023-04-06 20:11:26 +0900

    [FEAT] #13 - PostViewModel에서 이미지 리스트 요청 및 화면 테스트 완료

--- core/domain/src/main/java/com/core/domain/model/Image.kt
+++ core/domain/src/main/java/com/core/domain/model/Image.kt
@@ -1,11 +1,10 @@
 package com.core.domain.model
 
-import android.net.Uri
 import com.core.data.model.ImageSource
 
 data class Image(
     val id: Long?,
-    val imageUrl: Uri
+    val imageUrl: String
 )
 
 internal fun ImageSource.toImage() = Image(
--- feature/post/build.gradle.kts
+++ feature/post/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
-    id ("haroo.android.library")
+    id("haroo.android.library")
     id("haroo.android.library.compose")
+    id("haroo.android.hilt")
 }
 
 android {
     namespace = "com.feature.post"
+}
+
+dependencies {
+    implementation(project(":core:domain"))
+
+    implementation(libs.paging.compose)
 }

68ef38974fdf75fa7657ffe727d745f591c62189
Yeoonju/Jane <aurora32s@naver.com>
2023-04-06 13:54:55 +0900

    Revert "[FEAT] #11 - 게시글(post)를 위한 dao 추가"
    This reverts commit 524a64b048c13f8b3692715a17fad2fd6a7c7e22.

--- core/database/src/main/java/com/core/database/dao/PostDao.kt
+++ core/database/src/main/java/com/core/database/dao/PostDao.kt
@@ -1,34 +1,9 @@
 package com.core.database.dao
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import com.core.model.database.ImageEntity
-import com.core.model.database.PostEntity
 
 /**
  * 일별 기록과 관련된 Dao
  */
 @Dao
-interface PostDao {
-    // post 추가
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPost(post: PostEntity): Int
-
-    // image 추가
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertImages(images: List<ImageEntity>): Int
-
-    // 특정 년도/월/일의 post 요청
-    @Query("SELECT * FROM post WHERE year = :year AND month = :month AND day = :day")
-    suspend fun selectPostByDate(year: Int, month: Int, day: Int): List<PostEntity>
-
-    // 특정 년도/월의 post 요청
-    @Query("SELECT * FROM post WHERE year = :year AND month = :month")
-    suspend fun selectPostByMonth(year: Int, month: Int): List<PostEntity>
-
-    // 특정 post 의 image 요청
-    @Query("SELECT * FROM image WHERE post_id = :postId")
-    suspend fun selectImagesByPost(postId: Int): List<ImageEntity>
-}
+interface PostDao {}

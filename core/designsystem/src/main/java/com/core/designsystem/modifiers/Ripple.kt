97fa32df15d46b055774ff99f04315564299e3fd
aurora32s <aurora32s@naver.com>
2023-04-07 17:52:57 +0900

    [FEAT] #20 - Ripple이 되지 않는 clickable Modifier 추가

---
+++ core/designsystem/src/main/java/com/core/designsystem/modifiers/Ripple.kt
@@ -0,0 +1,22 @@
+package com.core.designsystem.modifiers
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.semantics.Role
+
+internal fun Modifier.noRippleClickable(
+    enabled: Boolean = true,
+    role: Role? = null,
+    onClick: () -> Unit
+): Modifier = composed {
+    clickable(
+        interactionSource = remember { MutableInteractionSource() },
+        indication = null,
+        enabled = enabled,
+        role = role,
+        onClick = onClick
+    )
+}

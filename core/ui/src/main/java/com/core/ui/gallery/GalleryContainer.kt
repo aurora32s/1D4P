e4782c6ef9ebf49ab3e1b745f581d3a60bd7d990
aurora32s <aurora32s@naver.com>
2023-04-07 11:08:42 +0900

    [FEAT] #13 - GalleryScreen Header 추가

--- core/ui/src/main/java/com/core/ui/gallery/GalleryContainer.kt
+++ core/ui/src/main/java/com/core/ui/gallery/GalleryContainer.kt
@@ -1,16 +1,22 @@
 package com.core.ui.gallery
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
-import com.core.designsystem.components.HarooButton
+import com.core.designsystem.components.BackAndRightButtonHeader
 import com.core.designsystem.components.SelectableImage
+import com.core.designsystem.theme.HarooTheme
 import com.core.model.feature.ImageUiModel
 
 private const val galleryColumn = 3
@@ -19,17 +25,23 @@ private const val galleryColumn = 3
 fun GalleryContainer(
     images: LazyPagingItems<ImageUiModel>, // gallery image 정보
     selectedImages: List<ImageUiModel>, // 선택된 이미지 정보
-    limit: Int, // 선택할 수 이미지 최대 개수
-    onImageSelect: (ImageUiModel) -> Unit // 이미지 선택 event
+    limit: Int = 0, // 선택할 수 이미지 최대 개수
+    onImageSelect: (ImageUiModel) -> Unit = {} // 이미지 선택 event
 ) {
-    Column {
-        Row {
-            if (selectedImages.isNotEmpty()) {
-                HarooButton(onClick = {}) {
-                    Text(text = "${selectedImages.size} 선택")
-                }
+    Column(
+        modifier = Modifier.background(
+            Brush.linearGradient(HarooTheme.colors.interactiveBackground)
+        )
+    ) {
+        BackAndRightButtonHeader(
+            title = "갤러리",
+            showButtonFlag = selectedImages.isNotEmpty(),
+            onBackPressed = { },
+            onClick = { },
+            content = {
+                Text(text = "${selectedImages.size}개 선택")
             }
-        }
+        )
         LazyVerticalGrid(
             columns = GridCells.Fixed(galleryColumn),
             contentPadding = PaddingValues(1.dp),

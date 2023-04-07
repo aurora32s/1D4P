192d5f23755fef933cc8acc1eea38d75289424fb
aurora32s <aurora32s@naver.com>
2023-04-07 15:13:38 +0900

    [FEAT] #20 - ModalBottomSheet Container 추가

--- feature/post/build.gradle.kts
+++ feature/post/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation(project(":core:domain"))
     implementation(project(":core:ui"))
     implementation(project(":core:model"))
+    implementation(project(":core:designsystem"))
 
     implementation(libs.paging.compose)
 }

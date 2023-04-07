b7998191661a33267113659a8e321d00a45ac8de
aurora32s <aurora32s@naver.com>
2023-04-07 10:32:38 +0900

    [FEAT] #13 - GalleryScreen 및 4개의 이미지만 선택 가능하도록 수정

--- feature/post/src/main/java/com/feature/post/PostScreen.kt
+++ feature/post/src/main/java/com/feature/post/PostScreen.kt
@@ -1,7 +1,29 @@
 package com.feature.post
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.core.ui.gallery.GalleryContainer
 
 @Composable
-fun PostScreen() {
+fun PostScreen(
+    postViewModel: PostViewModel = hiltViewModel()
+) {
+    val images = postViewModel.images.collectAsLazyPagingItems()
+    val selectedImages = postViewModel.selectedImages.collectAsState()
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        GalleryContainer(
+            images = images,
+            selectedImages = selectedImages.value,
+            limit = PostViewModel.IMAGE_SELECT_LIMIT,
+            onImageSelect = postViewModel::selectImage
+        )
+    }
 }

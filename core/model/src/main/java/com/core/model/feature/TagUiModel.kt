a2b54063ddf4b88686b75df38bfd726a893cd638
aurora32s <aurora32s@naver.com>
2023-04-08 17:12:43 +0900

    [FEAT] #23 - 태그를 입력할 수 있는 Tag Component 추가

--- core/model/src/main/java/com/core/model/feature/TagUiModel.kt
+++ core/model/src/main/java/com/core/model/feature/TagUiModel.kt
@@ -4,6 +4,6 @@ package com.core.model.feature
  * tag 정보를 위한 ui model
  */
 data class TagUiModel(
-    override val id: Long?,
+    override val id: Long? = null,
     val name: String
 ) : Model(id, CellType.TAG)
---
+++ core/ui/src/main/java/com/core/ui/tag/Tag.kt
@@ -0,0 +1,67 @@
+package com.core.ui.tag
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.core.designsystem.components.HarooButton
+import com.core.designsystem.components.HarooDivider
+import com.core.designsystem.components.HarooTextField
+import com.core.designsystem.theme.AllForMemoryTheme
+import com.core.model.feature.TagUiModel
+
+@Composable
+fun TagTextField(
+    modifier: Modifier = Modifier,
+    onAddTag: (TagUiModel) -> Unit
+) {
+    val tag = remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            HarooTextField(
+                modifier = Modifier.weight(1f),
+                value = tag.value,
+                onValueChange = { tag.value = it },
+                color = Color.Transparent,
+                placeHolder = "태그 입력...",
+                contentPadding = PaddingValues(horizontal = 8.dp)
+            )
+            HarooButton(
+                alpha = 0f,
+                onClick = { onAddTag(TagUiModel(name = tag.value)) }
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Add,
+                    contentDescription = "Add Tag"
+                )
+            }
+        }
+        HarooDivider()
+    }
+}
+
+@Composable
+@Preview(name = "basic tag text field")
+fun TagTextFieldPreview() {
+    AllForMemoryTheme {
+        TagTextField(
+            onAddTag = {}
+        )
+    }
+}

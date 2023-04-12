66b33095aa04c5577ee98543eafdb5fae9101407
aurora32s <aurora32s@naver.com>
2023-04-12 20:28:57 +0900

    [FEAT] #26 - LocalDate에서 날짜와 요일 반환 확장함수 추가

--- core/common/src/main/java/com/core/common/ext/LocalDateExtension.kt
+++ core/common/src/main/java/com/core/common/ext/LocalDateExtension.kt
@@ -1,6 +1,8 @@
 package com.core.common.ext
 
 import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.*
 
 /**
  * 연도와 월 출력
@@ -13,3 +15,13 @@ fun LocalDate.getYearMonth(
 
     return "$year${separator}${month.padStart(2)}"
 }
+
+/**
+ * 날짜 출력
+ */
+fun LocalDate.getDate(): String = dayOfMonth.padStart(2)
+
+/**
+ * 요일 출력(영어로)
+ */
+fun LocalDate.dayOfWeek(): String = dayOfWeek.getDisplayName(TextStyle.FULL, Locale.US)

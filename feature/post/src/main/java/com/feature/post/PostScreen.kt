a6d229b78b8383d29f95e3889bd1788414add73f
aurora32s <aurora32s@naver.com>
2023-04-13 15:55:25 +0900

    [FEAT] #26 - PostScreen의 UI 로직을 UiState에서 관리할 수 있도록 수정

--- feature/post/src/main/java/com/feature/post/PostScreen.kt
+++ feature/post/src/main/java/com/feature/post/PostScreen.kt
@@ -2,23 +2,21 @@ package com.feature.post
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.core.designsystem.components.*
+import com.core.designsystem.components.BackAndRightButtonHeader
+import com.core.designsystem.components.HarooBottomDrawer
+import com.core.designsystem.components.HarooTextField
+import com.core.designsystem.components.RemovableImage
 import com.core.designsystem.modifiers.onInteraction
 import com.core.designsystem.theme.HarooTheme
 import com.core.ui.date.YearMonthDayText
@@ -28,85 +26,66 @@ import com.core.ui.image.AsyncImageLazyRow
 import com.core.ui.tag.TagContainer
 import java.time.LocalDate
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun PostScreen(
-    postViewModel: PostViewModel = hiltViewModel(),
-    focusManager: FocusManager = LocalFocusManager.current,
-    isImeVisible: Boolean = WindowInsets.isImeVisible
+    postViewModel: PostViewModel = hiltViewModel()
 ) {
-    LaunchedEffect(Unit) {
-        postViewModel.getPost(2023, 4, 13)
-    }
-    val images = postViewModel.images.collectAsLazyPagingItems()
-    val selectedImages = postViewModel.selectedImages.collectAsState()
-    val tags = postViewModel.tags.collectAsState()
-    val bottomDrawerState = rememberHarooBottomDrawerState()
-    val content = postViewModel.content.collectAsState()
+    val postStateHolder = rememberPostScreenState(postViewModel = postViewModel)
+    PostScreen(postStateHolder = postStateHolder)
+}
 
-    BackHandler(enabled = bottomDrawerState.isShow.value) {
-        bottomDrawerState.hide()
+@Composable
+fun PostScreen(
+    postStateHolder: PostScreenStateHolder
+) {
+    // BottomDrawer 가 열려 있다면 Back 버튼 클릭 시, Drawer hide
+    BackHandler(enabled = postStateHolder.isBottomDrawer.value) {
+        postStateHolder.bottomDrawerHide()
     }
 
-    val interactionSource = remember { MutableInteractionSource() }
-    val isPress = interactionSource.collectIsPressedAsState()
-    val showTagTextField = remember { mutableStateOf(false) }
-
-    LaunchedEffect(key1 = isPress.value) {
-        // 1. A, B 클릭 시
-        if (isPress.value) {
-            // 2. C, D focus lost
-            focusManager.clearFocus()
-            // 3. D close
-            if (showTagTextField.value)
-                showTagTextField.value = false
-        }
-    }
-    LaunchedEffect(key1 = isImeVisible) {
-        if (isImeVisible.not() && showTagTextField.value) {
-            showTagTextField.value = false
-        }
-    }
+    postStateHolder.CollectImeVisible()
+    postStateHolder.CollectPressFlag()
 
     HarooBottomDrawer(
         modifier = Modifier
             .statusBarsPadding()
             .navigationBarsPadding()
             .imePadding(),
-        drawerState = bottomDrawerState,
+        drawerState = postStateHolder.bottomDrawerState,
         drawerContent = {
             DrawerGalleryContainer(
-                images = images,
-                selectedImages = selectedImages.value,
+                images = postStateHolder.images,
+                selectedImages = postStateHolder.selectedImages,
                 limit = PostViewModel.IMAGE_SELECT_LIMIT,
                 space = 2.dp,
-                onClose = { bottomDrawerState.hide() },
-                onImageSelect = {
-                    postViewModel.setImages(it)
-                    bottomDrawerState.hide()
-                }
+                onClose = postStateHolder::bottomDrawerHide,
+                onImageSelect = postStateHolder::addSelectedImage
             )
         }
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Brush.linearGradient(HarooTheme.colors.interactiveBackground))
+                .background(
+                    Brush.linearGradient(
+                        HarooTheme.colors.interactiveBackground
+                    )
+                )
         ) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .onInteraction(interactionSource)
+                    .onInteraction(postStateHolder.interactionSource)
                     .onFocusChanged {
                         // 4. C 클릭 시, D가 열려 있는 경우 close
-                        if (it.isFocused && showTagTextField.value)
-                            showTagTextField.value = false
+                        if (it.isFocused)
+                            postStateHolder.closeTagTextField()
                     }
             ) {
                 BackAndRightButtonHeader(
                     title = "글 작성",
                     onBackPressed = { },
-                    onClick = postViewModel::savePost
+                    onClick = postStateHolder::savePost
                 ) {
                     // TODO 새 글 작성 시에는 저장, 기존 글인 경우 수정정
                     Text(text = "저장")
@@ -120,14 +99,14 @@ fun PostScreen(
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(210.dp),
-                    images = selectedImages.value,
+                    images = postStateHolder.selectedImages,
                     space = 4.dp,
                     contentPadding = 12.dp,
                     content = { image ->
                         RemovableImage(
                             image = image,
                             contentScale = ContentScale.FillHeight,
-                            onRemove = postViewModel::removeImage
+                            onRemove = postStateHolder::removeImage
                         )
                     }
                 )
@@ -135,8 +114,8 @@ fun PostScreen(
                     modifier = Modifier.padding(
                         horizontal = 12.dp, vertical = 8.dp
                     ),
-                    value = content.value,
-                    onValueChange = postViewModel::setContent,
+                    value = postStateHolder.content,
+                    onValueChange = postStateHolder::setContent,
                     autoFocus = true,
                     alpha = 0.1f,
                     placeHolder = "내용을 입력해주세요...",
@@ -146,11 +125,11 @@ fun PostScreen(
             }
             TagContainer(
                 modifier = Modifier.padding(horizontal = 12.dp),
-                showTagTextFieldFlag = showTagTextField.value,
-                tags = tags.value,
-                onAddTag = postViewModel::addTag,
-                onRemoveTag = postViewModel::removeTag,
-                showTagTextField = { showTagTextField.value = true }
+                showTagTextFieldFlag = postStateHolder.showTagTextFieldFlag,
+                tags = postStateHolder.tags,
+                onAddTag = postStateHolder::addTag,
+                onRemoveTag = postStateHolder::removeTag,
+                showTagTextField = postStateHolder::showTagTextField
             )
             BottomAppBar(
                 modifier = Modifier
@@ -158,20 +137,12 @@ fun PostScreen(
                 backgroundColor = Color.Transparent
             ) {
                 GalleryListContainer(
-                    images = images,
+                    images = postStateHolder.images,
                     space = 8.dp,
-                    selectedImages = selectedImages.value,
+                    selectedImages = postStateHolder.selectedImages,
                     limit = PostViewModel.IMAGE_SELECT_LIMIT,
-                    onClickAddButton = {
-                        if (showTagTextField.value) showTagTextField.value = false
-                        focusManager.clearFocus()
-                        bottomDrawerState.show()
-                    },
-                    onImageSelect = {
-                        if (showTagTextField.value) showTagTextField.value = false
-                        focusManager.clearFocus()
-                        postViewModel.selectImage(it)
-                    }
+                    onClickAddButton = postStateHolder::showBottomDrawer,
+                    onImageSelect = postStateHolder::setSelectedImage
                 )
             }
         }

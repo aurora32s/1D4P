87f1d9930fb7a420172643918cd1e7ad838e50e6
aurora32s <aurora32s@naver.com>
2023-04-15 17:44:38 +0900

    [FEAT] #32 - HomeScreen MonthlyDivider 코드 정리

--- core/designsystem/src/main/java/com/core/designsystem/util/Resources.kt
+++ core/designsystem/src/main/java/com/core/designsystem/util/Resources.kt
@@ -1,10 +1,11 @@
 package com.core.designsystem.util
 
-import androidx.annotation.DimenRes
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 
 @Composable
 fun getString(@StringRes id: Int) = stringResource(id = id)
+
+@Composable
+fun getString(@StringRes id: Int, vararg values: Any) = stringResource(id = id, formatArgs = values)

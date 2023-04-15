47a1a745a420fa68650963620f8f12622a0b1dd7
aurora32s <aurora32s@naver.com>
2023-04-15 12:29:30 +0900

    [FEAT] #32 - post paging 정보 요청 usecase 생성

--- core/domain/src/main/java/com/core/domain/post/GetPostPageByMonth.kt
+++ core/domain/src/main/java/com/core/domain/post/GetPostPageByMonth.kt
@@ -1,10 +1,21 @@
 package com.core.domain.post
 
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.core.data.post.PostRepository
+import com.core.model.domain.Post
+import com.core.model.domain.toPost
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
 /**
  * 월별 기록 paging 요청 use case
  */
-class GetPostPageByMonth {
-    suspend operator fun invoke(year: Int, month: Int) {
-
+class GetPostPageByMonth @Inject constructor(
+    private val postRepository: PostRepository
+) {
+    operator fun invoke(year: Int, month: Int): Flow<PagingData<Post>> {
+        return postRepository.getPostPaging(year, month).map { it.map { post -> post.toPost() } }
     }
 }

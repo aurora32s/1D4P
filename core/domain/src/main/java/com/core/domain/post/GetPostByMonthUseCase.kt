7d0043c2dd65d354335159acd1585aac20752a56
aurora32s <aurora32s@naver.com>
2023-04-15 23:30:55 +0900

    [FEAT] #34 - 특정 Monthly 데이터 요청 Business 로직 추가

--- core/domain/src/main/java/com/core/domain/post/GetPostByMonthUseCase.kt
+++ core/domain/src/main/java/com/core/domain/post/GetPostByMonthUseCase.kt
@@ -1,10 +1,18 @@
 package com.core.domain.post
 
+import com.core.data.post.PostRepository
+import com.core.model.domain.Post
+import com.core.model.domain.toPost
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
 /**
  * 월별 기록 요청 use case
  */
-class GetPostByMonthUseCase {
-    suspend operator fun invoke(year: Int, month: Int) {
-
+class GetPostByMonthUseCase @Inject constructor(
+    private val postRepository: PostRepository
+) {
+    suspend operator fun invoke(year: Int, month: Int): List<Post> {
+        return postRepository.getPosts(year, month).map { posts -> posts.toPost() }
     }
 }

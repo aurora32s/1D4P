c11400ba7fd5094aacf25320e6b36c677bae0b60
aurora32s <aurora32s@naver.com>
2023-04-16 19:45:55 +0900

    [FEAT] #34 - Custom Radio Button 추가

--- core/designsystem/src/main/java/com/core/designsystem/components/Button.kt
+++ core/designsystem/src/main/java/com/core/designsystem/components/Button.kt
@@ -1,5 +1,6 @@
 package com.core.designsystem.components
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -8,17 +9,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.core.designsystem.modifiers.noRippleClickable
 import com.core.designsystem.theme.AllForMemoryTheme
 import com.core.designsystem.theme.HarooTheme
+import kotlin.math.abs
 
 @Composable
 fun HarooButton(
@@ -85,6 +90,50 @@ fun ButtonPreview() {
             ) {
                 Text(text = "추가")
             }
+        }
+    }
+}
+
+@Composable
+fun HarooRadioButton(
+    modifier: Modifier = Modifier,
+    contentColor: Color = HarooTheme.colors.text,
+    radioSize: Dp = 32.dp,
+    selected: Boolean = false,
+    onSelected: () -> Unit
+) {
+    val currentState = remember(selected) {
+        MutableTransitionState(selected).apply { targetState = selected.not() }
+    }
+    val transition = updateTransition(targetState = currentState, label = null)
+    val radioOffset = transition.animateFloat(
+        transitionSpec = {
+            tween(delayMillis = 0, durationMillis = 300, easing = LinearEasing)
+        }, label = ""
+    ) {
+        if (it.currentState) 1f else 0f
+    }
+
+    HarooButton(
+        modifier = modifier
+            .height(radioSize)
+            .width(radioSize * 2),
+        onClick = onSelected,
+        contentPadding = PaddingValues()
+    ) {
+        HarooSurface(
+            modifier = Modifier
+                .fillMaxHeight()
+                .aspectRatio(1f)
+                .graphicsLayer {
+                    translationX = radioOffset.value * radioSize.value * 2 - radioSize.value
+                    scaleX = 1 - abs(0.5 - radioOffset.value).toFloat()
+                    scaleY = 1 - abs(0.5 - radioOffset.value).toFloat()
+                },
+            shape = MaterialTheme.shapes.small,
+            alpha = 1f
+        ) {
+
         }
     }
 }

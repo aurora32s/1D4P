24e9a763ba1c46b05cf8fb064c01355f4050bb22
aurora32s <aurora32s@naver.com>
2023-04-17 17:08:20 +0900

    [FEAT] #36 - MonthlyHeader 하드 코딩 부분 제거

---
+++ feature/monthly/src/main/java/com/feature/monthly/ui/dimens.kt
@@ -0,0 +1,12 @@
+package com.feature.monthly.ui
+
+import androidx.compose.ui.unit.dp
+
+internal object Dimens {
+    val monthAndNameDefaultPadding = 16.dp
+    val monthlyInfoVerticalSpace = 16.dp
+    val monthlyInfoHorizontalSpace =30.dp
+
+    val monthlyCalendarVerticalSpace = 8.dp
+    val monthlyCalendarHorizontalSpace = 8.dp
+}

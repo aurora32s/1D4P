222eafe582385bceb6b9b6dd1aa336fe1a67fe51
aurora32s <aurora32s@naver.com>
2023-04-20 20:01:57 +0900

    [FEAT] #24 - Post 추가(수정) 성공 & 실패 처리

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -8,6 +8,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:common"))
     implementation(project(":core:datastore"))
     implementation(project(":core:database"))
     implementation(project(":core:model"))

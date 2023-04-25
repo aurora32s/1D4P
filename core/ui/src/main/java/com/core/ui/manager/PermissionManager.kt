10bf6695eaa2f2ac780d7a10923b16b9c850be59
aurora32s <aurora32s@naver.com>
2023-04-25 13:51:17 +0900

    [FEAT] #19 - PermissionRational Component 추가 및 기본 Permission 테스트

--- core/ui/src/main/java/com/core/ui/manager/PermissionManager.kt
+++ core/ui/src/main/java/com/core/ui/manager/PermissionManager.kt
@@ -8,7 +8,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.outlined.Image
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.core.content.ContextCompat
 import com.core.ui.R
@@ -23,14 +23,17 @@ class PermissionManager(
     private val _isNeedPermissionRationale = MutableStateFlow<List<Permission>>(emptyList())
     val isNeedPermissionRationale: StateFlow<List<Permission>> = _isNeedPermissionRationale.asStateFlow()
 
-    private var currentRequestCallback: (() -> Unit)? = null
+    private var currentRequestCallback: ((Boolean) -> Unit)? = null
 
     // permission 요청
-    fun requestPermission(permission: List<String>, cb: () -> Unit) {
+    fun requestPermission(permission: List<String>, cb: (Boolean) -> Unit) {
         // 1. Grant 되지 않는 permission 들만 filtering
         val notGrantedPermissions = permission.filterNotGranted()
         // 2. 모든 Permission 이 이미 승인 되어 있는 경우 에는 바로 cb 호출
-        if (notGrantedPermissions.isEmpty()) cb()
+        if (notGrantedPermissions.isEmpty()) {
+            cb(true)
+            return
+        }
         // 3. 그러지 않을 경우, Rational show 요청
         currentRequestCallback = cb
         _isNeedPermissionRationale.value = permission.map {
@@ -50,21 +53,22 @@ class PermissionManager(
 
     /**
      * 권한 설명이 들어간 Rational 에서 승인 / 거부 버튼 클릭
-     * @param grantOrDeny true 인 경우 grant, false 인 경우 deny
      */
-    fun responseRational(grantOrDeny: Boolean) {
-        if (grantOrDeny) {
-            val permissions = _isNeedPermissionRationale.value.map { it.permission }.toTypedArray()
-            _isNeedPermissionRationale.value = emptyList()
-            requestPermissionLauncher?.launch(permissions)
-        } else currentRequestCallback?.invoke()
+    fun responseGrantRational() {
+        val permissions = _isNeedPermissionRationale.value.map { it.permission }.toTypedArray()
+        _isNeedPermissionRationale.value = emptyList()
+        requestPermissionLauncher?.launch(permissions)
+    }
+
+    fun responseDenyRational() {
+        currentRequestCallback?.invoke(false)
     }
 
     private val requestPermissionLauncher =
         (context as? ComponentActivity)?.registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) { results ->
-            currentRequestCallback?.invoke()
+            currentRequestCallback?.invoke(results.all { it.value })
         }
 }
 
@@ -87,6 +91,7 @@ private val Permissions = mapOf(
         permission = externalStoragePermission,
         title = R.string.external_storage_title,
         description = R.string.external_storage_desc,
-        icon = Icons.Default.Image
+        icon = Icons.Outlined.Image
     )
 )
+

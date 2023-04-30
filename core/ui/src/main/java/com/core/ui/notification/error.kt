da6b0b970381d61a35ac45bebfb6f4cdf89388a9
aurora32s <aurora32s@naver.com>
2023-04-30 23:12:45 +0900

    [FEAT] #41 - ErrorNotification UI 수정

--- core/ui/src/main/java/com/core/ui/notification/error.kt
+++ core/ui/src/main/java/com/core/ui/notification/error.kt
@@ -1,8 +1,11 @@
 package com.core.ui.notification
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,6 +13,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.core.common.ext.fullDisplayName
 import com.core.designsystem.components.HarooButton
@@ -29,7 +33,7 @@ fun PostRequestErrorNotification(
         modifier = modifier,
         title = date.fullDisplayName(),
         description = getString(id = R.string.error_post_load_desc),
-        count = 5,
+        count = 10,
         onFinishCount = onFinishCount
     )
 }
@@ -58,12 +62,18 @@ fun ErrorNotification(
             modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = title)
-            Text(text = description)
+            Image(
+                modifier = Modifier.size(65.dp),
+                painter = painterResource(id = R.drawable.app_icon),
+                contentDescription = "Post request fail"
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(text = title, style = MaterialTheme.typography.h5)
+            Text(text = description, style = MaterialTheme.typography.subtitle1)
             Text(text = getString(id = R.string.error_retry_msg))
             Spacer(modifier = Modifier.height(32.dp))
             HarooButton(onClick = onFinishCount) {
-                Text(text = "${extraCount.value}초")
+                Text(text = "${extraCount.value}초", style = MaterialTheme.typography.h6)
             }
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = getString(id = R.string.error_btn_desc, count))
